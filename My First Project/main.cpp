d5ddb06982f221375e17e9d63eabe04cdb91bacb
bergum1 <bergum1@tritons.iowacentral.edu>
2022-08-29 10:30:33 -0500

    Initial Commit

---
+++ My First Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  My First Project
+//
+//  Created by Austin Bergum on 8/29/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
